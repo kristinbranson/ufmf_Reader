17e3bd57331e9db7a98195b249b5bc6c24502037
Austin Edwards <edwardsa@janelia.hhmi.org>
2014-11-19 14:18:23 -0500

    created new class files for FrameIndex and KeyFrameIndex, removed these from UFMFFile

---
+++ read_ufmf/src/read_ufmf/KeyFrameIndex.java
@@ -0,0 +1,51 @@
+package read_ufmf;
+
+import java.io.IOException;
+import ucar.unidata.io.RandomAccessFile;
+
+/**
+ * Keyframe index class
+ */
+
+public class KeyFrameIndex {
+
+	private String DICT_START_CHAR = "d";
+	private byte[] buf = new byte[1024];
+	
+	public FrameIndex meanindex;
+	
+	public KeyFrameIndex(RandomAccessFile raf) throws IOException {
+			
+			String chunktype = Character.toString((char) raf.read());
+			
+			if (!chunktype.trim().equals(DICT_START_CHAR)){
+				System.out.printf("Error reading index: dictionary does not start with '%s'.", DICT_START_CHAR);
+				}
+			
+			// get number of keys in frames
+			int nkeys = raf.read();
+			
+			for (int j = 0; j < nkeys; j++) {
+				
+				//read length of key
+				int l = raf.read();
+				
+				//read key
+				raf.read(buf,0,l+1);
+				String key = new String(buf,"UTF-8");
+				
+				//read chunktype
+				chunktype = Character.toString((char) raf.read());
+				
+				if (chunktype.trim().equals(DICT_START_CHAR)){
+					
+					raf.unread();
+					
+					if (key.trim().equals("mean")) {
+						meanindex = new FrameIndex(raf);
+					
+					}
+				}
+			}
+	}
+}
